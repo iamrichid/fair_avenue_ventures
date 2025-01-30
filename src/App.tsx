8dc18f8b63357434d0aee412cdb9614f993bdbbc
iamrichid <rkklinks@gmail.com>
2025-01-30 12:43:15 +0000

    push

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header/Header'
 import Services from './components/Services'
-import Testimonial10 from './components/Testimonial'
-import Testimonial from './components/Testimonial'
+
 
 function App() {
  
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -55,7 +55,7 @@ const Header = () => {
                                 id=""
                                 placeholder="Enter email address"
                                 className="block w-full px-4 py-4 text-gray-900 placeholder-gray-900 bg-transparent border border-gray-400 outline-none focus:border-gray-900 focus:ring-1 focus:ring-gray-900 rounded-xl sm:border-none sm:focus:ring-0 sm:focus:border-transparent"
-                                required=""
+                                
                             />
                             <div className="mt-4 sm:mt-0 sm:absolute sm:inset-y-0 sm:right-0 sm:flex sm:items-center sm:pr-2">
                                 <button type="submit" className="inline-flex px-6 py-3 text-lg font-bold text-white transition-all duration-200 bg-gray-900 rounded-lg focus:outline-none focus:bg-gray-600 font-pj hover:bg-gray-600">Get a catalog of our services</button>
